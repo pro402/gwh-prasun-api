0dc21e15f6ba7ec90e65ae86ab327d762e4ee5ed
pro402 <maitypro402@gmail.com>
2023-04-07 12:31:42 +0530

    thanks

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p> Hello Prasun </p>"
+    return "<p> Thanks for the lesson!!! </p>"
 
 if __name__=="__main__":
     app.run(debug=True)
